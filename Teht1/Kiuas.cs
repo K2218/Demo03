6dbb80e32edf0ac4035d2deb71f1cd06941b6b1e
K2218 <K2218@student.jamk.fi>
2016-01-27 11:04:38 +0200

    Includes Car application and task 1

---
+++ Teht1/Kiuas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Kiuas
+    {
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public bool IsOn { get; set; }
+
+        public Kiuas(int temperature)
+        {
+            Temperature = temperature;
+        }
+
+        public void IncreaseTemperature()
+        {
+            Temperature += 5; 
+        }
+
+        public void DecreaseTemperature()
+        {
+            Temperature -= 5;
+        }
+
+        //Print properties to console
+        public void PrintData()
+        {
+            Console.WriteLine("Kiuas properties:");
+            Console.WriteLine("- Temperature {0}", Temperature);
+            Console.WriteLine("- Humidity {0}", Humidity);
+            Console.WriteLine("- Kiuas is on: {0}", IsOn);
+        }
+
+        //Method returns obejct properties in a one string
+        public override string ToString()
+        {
+            return "temp = " + Temperature + " Humi = " + Humidity + " IsOn = " + IsOn;
+        }
+    }
+}
---
+++ Teht1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = 70;
+            Kiuas kiuas = new Kiuas(temperature);
+            kiuas.IsOn = true;
+            kiuas.IncreaseTemperature();
+            kiuas.Humidity = 50;
+            kiuas.PrintData();
+
+            Console.WriteLine("Temperature =" + kiuas.Temperature);
+
+            Console.WriteLine(kiuas.ToString());
+            Console.ReadLine();
+        }
+    }
+}
